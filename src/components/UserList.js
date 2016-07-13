9cae96274ee0a3c51c8d40f1b077c93ea2dc284f
James Simpson <jamess@ca.ibm.com>
2016-07-12 20:23:08 -0400

    Makes baseline empty branch.

--- src/components/UserList.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import style from '../chat.less';
-
-export default ({users}) => (
-	<ul className={style.userList}>
-		{ users.map(u => <li key={u.id}>{u.name}</li>)}
-	</ul>
-);
--- src/constants.js
+++
@@ -1,5 +0,0 @@
-import { constants } from './lib/socketChat';
-
-export const CHAT_URL = "http://chat.jamessimpson.ca";
-export const SOCKET_TYPE = 'SOCKET';
-export { constants as CHAT_CONSTANTS };
